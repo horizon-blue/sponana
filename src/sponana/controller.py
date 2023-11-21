a384de903545e3e0444d348a4208959f77e531e5
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-11-20 19:17:22 -0500

    Move teleop code to file

--- src/sponana/controller.py
+++ src/sponana/controller.py
@@ -1,11 +1,27 @@
-from pydrake.all import DiagramBuilder, StateInterpolatorWithDiscreteDerivative
+from typing import NamedTuple, Optional
+
+from pydrake.all import (
+    Diagram,
+    DiagramBuilder,
+    JointSliders,
+    Meshcat,
+    MultibodyPlant,
+    StateInterpolatorWithDiscreteDerivative,
+)
 
 
-def make_spot_controller():
+def make_spot_controller(
+    spot_plant: MultibodyPlant,
+    use_teleop: bool = True,
+    meshcat: Optional[Meshcat] = None,
+) -> Diagram:
     """
     Create and return a Spot controller that output the state configuration
     (which can be feed into the hardware station to actually move the robot)
     """
+    if use_teleop and not meshcat:
+        raise ValueError("A meshcat object is required to use teleop")
+
     builder = DiagramBuilder()
 
     # Convert positions to desired state
@@ -14,19 +30,35 @@ def make_spot_controller():
         StateInterpolatorWithDiscreteDerivative(
             # TODO: split the control of end effector (the gripper) from the
             # control of the base of Spot
-            num_positions=10,
+            num_positions=spot_plant.num_positions(),
             time_step=0.05,
             suppress_initial_transient=True,
         ),
     )
 
+    if use_teleop:
+        # Pose Sliders
+        teleop = builder.AddNamedSystem(
+            "teleop",
+            JointSliders(
+                meshcat,
+                spot_plant,
+                spot_plant.GetPositions(spot_plant.CreateDefaultContext()),
+                step=0.05,
+                decrement_keycodes=["ArrowLeft", "ArrowDown", "KeyA"] + [""] * 7,
+                increment_keycodes=["ArrowRight", "ArrowUp", "KeyD"] + [""] * 7,
+            ),
+        )
+        builder.Connect(teleop.get_output_port(), position_to_state.get_input_port())
+
     # Export I/O ports
-    builder.ExportInput(position_to_state.get_input_port(), "position")
+    if not use_teleop:
+        builder.ExportInput(position_to_state.get_input_port(), "position")
     builder.ExportOutput(position_to_state.get_output_port(), "desired_state")
 
     # Finalize the diagram
     spot_controller = builder.Build()
-    spot_controller.set_name("spot_controller")
+    spot_controller.set_name("spot_controller" + ("_teleoped" if use_teleop else ""))
     return spot_controller
 
 
