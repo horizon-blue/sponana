834d76744e8caece53609a5027519c5ecd7000ba
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-11-22 15:38:55 -0500

    Add debugger to clutter scene as well

--- src/sponana/perception/banana_spotter.py
+++ src/sponana/perception/banana_spotter.py
@@ -46,21 +46,10 @@ class BananaSpotter(LeafSystem):
         _, has_banana = self._find_and_locate_banana(context)
         output[0] = has_banana
 
-    def _plot_images(self, context: Context):
-        """for debeugging"""
-        color_image = self.EvalAbstractInput(context, 0).get_value()
-        depth_image = self.EvalAbstractInput(context, 1).get_value()
-        plt.figure(figsize=(10, 4))
-        plt.subplot(1, 2, 1)
-        plt.imshow(color_image.data)
-        plt.subplot(1, 2, 2)
-        plt.imshow(depth_image.data)
-
     @cache
     def _find_and_locate_banana(self, context: Context) -> (RigidTransform, bool):
         # color_image = self.EvalAbstractInput(context, 0).get_value()
         # depth_image = self.EvalAbstractInput(context, 1).get_value()
-        self._plot_images(context)
         banana_pose = RigidTransform()
         has_banana = not not banana_pose
         return banana_pose, has_banana
