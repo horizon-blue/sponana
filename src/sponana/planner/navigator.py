ebe2c2e611f75321bdb025d1207f6084179e0c6b
Rachel Ma <rachelma80000@github.com>
2023-11-28 00:00:56 -0500

    assertion error

--- src/sponana/planner/navigator.py
+++ src/sponana/planner/navigator.py
@@ -11,7 +11,7 @@ from pydrake.all import (
     State,
 )
 
-from ..rrt_2_incorrect_collision_check_version import basic_rrt, rrt_test
+from ..rrt_2 import basic_rrt, rrt_test
 """
 def rrt_planner_dummy():
     Q, Q_split_arr = rrt_test()
@@ -87,7 +87,7 @@ class Navigator(LeafSystem):
 
     def get_target_position_input_port(self):
         return self.get_input_port(1)
-    """
+   
     def _execute_trajectory(self, context: Context, state: State):
         #for executing the trajectory calculated after RRT
         current_position = self.get_spot_state_input_port().Eval(context)[:3]
@@ -114,7 +114,7 @@ class Navigator(LeafSystem):
         # initial state
         state.set_value(self._base_position, trajectory[0])
         state.set_value(self._traj_idx, [0])
-        """
+        
     def _plan_trajectory(self, context: Context, state: State):
         """for just moving spot to a q_sample position for collision checks in RRT"""
         current_position = self.get_spot_state_input_port().Eval(context)[:3]
@@ -122,7 +122,7 @@ class Navigator(LeafSystem):
         # target_position = self.get_target_position_input_port().Eval(context)
         # target_position = [2.4, 1.15, 1.65]
 
-        trajectory = dummmy_planner #check_collision_move_spot()
+        trajectory = dummmy_planner() #check_collision_move_spot()
         if self._meshcat:
             for t, pose in enumerate(trajectory):
                 # convert position to pose for plotting
