4a09a35f10ec47f636160654eff9e4247f5d7f34
George Matheos <georgematheos@gmail.com>
2023-11-28 20:02:41 -0500

    bug fix

--- src/sponana/controller/position_combiner.py
+++ src/sponana/controller/position_combiner.py
@@ -14,6 +14,7 @@ class PositionCombiner(LeafSystem):
         # I/O
         # FIXME: allow it to be 10 for now becuase I havn't figured out how to get rid
         # of extra joints in the slider
+        self.use_teleop = use_teleop
         self.DeclareVectorInputPort("base_position", 10 if use_teleop else 3)
         self.DeclareVectorInputPort("arm_position", 10 if use_teleop else 7)
         self.DeclareVectorOutputPort("position", 10, self._combine_position)
@@ -25,6 +26,9 @@ class PositionCombiner(LeafSystem):
         return self.get_input_port(1)
 
     def _combine_position(self, context: Context, output: BasicVector):
-        base_position = self.get_base_position_input_port().Eval(context)[:3]
-        arm_position = self.get_arm_position_input_port().Eval(context)[3:]
+        base_position = self.get_base_position_input_port().Eval(context)
+        arm_position = self.get_arm_position_input_port().Eval(context)
+        if self.use_teleop:
+            base_position = base_position[:3]
+            arm_position = arm_position[3:]
         output.SetFromVector([*base_position, *arm_position])
