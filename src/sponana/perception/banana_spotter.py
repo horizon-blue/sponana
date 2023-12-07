f91b391cc1678060901e18648977792eb5554c8a
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-12-07 05:18:51 -0500

    Set up periodic state update for the placeholder perception system

--- src/sponana/perception/banana_spotter.py
+++ src/sponana/perception/banana_spotter.py
@@ -8,11 +8,12 @@ from pydrake.all import (
     LeafSystem,
     RgbdSensor,
     RigidTransform,
+    State,
 )
 
 
 class BananaSpotter(LeafSystem):
-    def __init__(self, camera: RgbdSensor, num_tables: int = 0):
+    def __init__(self, camera: RgbdSensor, num_tables: int = 0, time_step: float = 0.1):
         super().__init__()
         self._camera = camera
 
@@ -34,12 +35,19 @@ class BananaSpotter(LeafSystem):
             )
 
         # Output ports
-        self.DeclareAbstractOutputPort(
-            "banana_pose",
-            lambda: AbstractValue.Make(RigidTransform()),
-            self._locate_banana,
+        self._found_banana = self.DeclareDiscreteState(1)
+        self.DeclareStateOutputPort("found_banana", self._found_banana)
+        self._banana_pose = self.DeclareAbstractState(
+            AbstractValue.Make(RigidTransform())
         )
-        self.DeclareVectorOutputPort("found_banana", 1, self._find_banana)
+        self.DeclareStateOutputPort("banana_pose", self._banana_pose)
+
+        self.DeclareInitializationUnrestrictedUpdateEvent(self._initialize_state)
+        self.DeclarePeriodicUnrestrictedUpdateEvent(
+            period_sec=time_step,
+            offset_sec=0.0,
+            update=self._try_find_banana,
+        )
 
     def get_check_banana_input_port(self):
         return self.GetInputPort("check_banana")
@@ -62,26 +70,24 @@ class BananaSpotter(LeafSystem):
     def get_found_banana_output_port(self):
         return self.GetOutputPort("found_banana")
 
-    def _locate_banana(self, context: Context, output: AbstractValue):
-        banana_pose, _ = self._find_and_locate_banana(context)
-        output.set_value(banana_pose)
+    def _initialize_state(self, context: Context, state: State):
+        state.get_mutable_discrete_state().set_value(self._found_banana, [0])
+        self._set_banana_pose(state, RigidTransform())
 
-    def _find_banana(self, context: Context, output: BasicVector):
-        check_banana = self.get_check_banana_input_port().Eval(context)
-        if check_banana == 1:
-            _, found_banana = self._find_and_locate_banana(context)
-            output[0] = found_banana
-        else:
-            output[0] = 0
+    def _set_banana_pose(self, state: State, pose: RigidTransform):
+        state.get_mutable_abstract_state(self._banana_pose).set_value(pose)
 
-    @cache
-    def _find_and_locate_banana(self, context: Context) -> (RigidTransform, bool):
-        # color_image = self.EvalAbstractInput(context, 0).get_value()
-        # depth_image = self.EvalAbstractInput(context, 1).get_value()
-        check_banana = self.get_check_banana_input_port().Eval(context)
-        if check_banana == 1:
-            banana_pose = RigidTransform()
-            found_banana = not not banana_pose
-            return banana_pose, found_banana
-        else:
-            return [], 0
+    def _try_find_banana(self, context: Context, state: State):
+        should_check_banana = self.get_check_banana_input_port().Eval(context)
+        if not should_check_banana:
+            return
+
+        color_image = self.EvalAbstractInput(context, 0).get_value()
+        depth_image = self.EvalAbstractInput(context, 1).get_value()
+        # TODO: fill in the actual perception module and delete the following lines
+        plt.figure(figsize=(10, 4))
+        plt.subplot(1, 2, 1)
+        plt.imshow(color_image.data)
+        plt.subplot(1, 2, 2)
+        plt.imshow(depth_image.data)
+        plt.show()
