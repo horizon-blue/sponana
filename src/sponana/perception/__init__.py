f815c10078e22124afe6364dac9662de0b877c5d
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-12-09 02:58:58 -0500

    Fall back to use dummy banana spotter when bayes3D is not installed

--- src/sponana/perception/__init__.py
+++ src/sponana/perception/__init__.py
@@ -1,3 +1,13 @@
-from .banana_spotter_bayes3d import BananaSpotterBayes3D
+import warnings
+
 from .dummy_banana_spotter import DummyBananaSpotter
 from .utils import add_body_pose_extractor, add_camera_pose_extractor
+
+try:
+    from .banana_spotter_bayes3d import BananaSpotterBayes3D
+except ImportError:
+    warnings.warn(
+        "Failed to import banana spotter. Have you installed bayes3d? "
+        "Falling back to use the dummy banana spotter..."
+    )
+    BananaSpotterBayes3D = DummyBananaSpotter
