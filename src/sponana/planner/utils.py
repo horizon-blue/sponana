a6b68c9b9493f5adf4ab2f7179d17b77924c5a55
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-12-09 14:57:38 -0500

    Improve trajectory visuals

--- src/sponana/planner/utils.py
+++ src/sponana/planner/utils.py
@@ -4,12 +4,19 @@ from pydrake.all import Meshcat, RigidTransform, RotationMatrix
 from .rrt import ConfigType
 
 
-def visualize_path(path: list[ConfigType], meshcat: Meshcat, offset_idx: int = 0):
+def visualize_path(
+    path: list[ConfigType], meshcat: Meshcat, offset_idx: int = 0, **kwargs
+):
     for i, pose in enumerate(path):
         pose = RigidTransform(RotationMatrix.MakeZRotation(pose[2]), [*pose[:2], 0.0])
-        opacity = 0.2
+        opacity = 1 if i == len(path) - 1 else 0.2
+        length = 0.3 if i == len(path) - 1 else 0.125
         AddMeshcatTriad(
-            meshcat, f"trajectory_{i + offset_idx}", X_PT=pose, opacity=opacity
+            meshcat,
+            f"trajectory_{i + offset_idx}",
+            X_PT=pose,
+            opacity=opacity,
+            length=length,
         )
 
 
